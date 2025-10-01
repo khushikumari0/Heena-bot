2d7991c2bfce3215d3413baf9369496e0e452489
khushikumari0 <khushikumari009921@gmail.com>
2025-10-01 15:00:38 +0530

    Update antiout.js

--- Priyansh/events/antiout.js
+++ Priyansh/events/antiout.js
@@ -15,7 +15,7 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
   api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
    if (error) {
-    api.sendMessage(`Neha Thakur Ke Permission Ke Bina Bhag Nhi Sakte ${name} Group Mai Lagta Hai Usne Mujhe Block Kardiya😥 :( `, event.threadID)
+    api.sendMessage(`𝐊𝐇𝐔𝐒𝐇𝐈 𝐊𝐔𝐌𝐀𝐑𝐈 Ke Permission Ke Bina Bhag Nhi Sakte ${name} Group Mai Lagta Hai Usne Mujhe Block Kardiya😥 :( `, event.threadID)
    } else api.sendMessage(`Neha Thakur Ke Permission Ke Bina Bhag Ke Jaane Ka Nhi, ${name} Baby, Dekho Phir Se Add Kardiya Aapko`, event.threadID);
   })
  }
